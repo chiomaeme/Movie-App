4ff9978904b072c08db582b6eb6014340b2d6657
Chioma <96903876+chiomaeme@users.noreply.github.com>
2025-07-16 23:56:46 -0600

    Cleanup of nativewind package setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
     presets: [require("nativewind/preset")],
     theme: {
-  extend: {},
+  extend: {
+    colors: {
+      primary: "#030014"
+    }
+  },
 },
 plugins: [],
 }
